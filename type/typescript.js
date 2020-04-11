98270d91e6108682ebbb56f1ed22fad7de0b186d
bryan <bryan.lee.jh@gmail.com>
2020-04-12 01:06:10 +0800

    types

--- type/typescript.js
+++ type/typescript.js
@@ -1,3 +1,22 @@
-var sum = function (a, b) {
-    return a + b;
+// const sum = (a: number, b: number) => {
+//   return a + b;
+// }
+// const answer = sum(4, 5);
+// console.log(answer);
+// boolean
+var isCool = true;
+// number
+var age = 56;
+// string
+var eyeColor = 'brown';
+var favouriteQuote = "I'm not old I'm " + age;
+// array
+var pets = ['cat', 'dog'];
+var pets2 = ['lion', 'dragon'];
+// object
+var wizard = {
+    a: 'John'
 };
+// null and undefined
+var meh = undefined;
+var noo = null;
--- type/typescript.ts
+++ type/typescript.ts
@@ -1,7 +1,28 @@
-const sum = (a: number, b: number) => {
-  return a + b;
+// const sum = (a: number, b: number) => {
+//   return a + b;
+// }
+// const answer = sum(4, 5);
+// console.log(answer);
+
+// boolean
+let isCool: boolean = true;
+
+// number
+let age: number = 56;
+
+// string
+let eyeColor: string = 'brown';
+let favouriteQuote: string = `I'm not old I'm ${age}`;
+
+// array
+let pets: string[] = ['cat', 'dog'];
+let pets2: Array<string> = ['lion', 'dragon'];
+
+// object
+let wizard: object = {
+  a: 'John'
 }
 
-const answer = sum(4, 5);
-
-console.log(answer);
+// null and undefined
+let meh: undefined = undefined;
+let noo: null = null;
